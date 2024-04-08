3b3056b8abdc8f46e68d896123ec5643c7c4d813
Kyle <kylezhengkz@gmail.com>
2024-04-08 12:59:02 -0400

    reformat

--- login.php
+++ login.php
@@ -18,11 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         
         if (password_verify($_POST["password"], $user["password_hash"])) {
             session_start();
-            
             session_regenerate_id();
-            
             $_SESSION["user_id"] = $user["id"];
-            
             header("Location: index.php");
             exit;
         }
@@ -51,7 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <label for="email">email</label>
         <input type="email" name="email" id="email"
                value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
-        
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
         
